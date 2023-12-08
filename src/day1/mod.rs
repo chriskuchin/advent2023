3b6e16d8fdbda9374d4bd77b6e7db2a0dbef2dc0
Chris Kuchin <github@kchn.dev>
2023-12-07 23:36:56 -0700

    Day 1 Complete

---
+++ src/day1/mod.rs
@@ -0,0 +1,103 @@
+use regex::Regex;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn solve() {
+    let mut part1: i32 = 0;
+    let mut part2: i32 = 0;
+    if let Ok(lines) = read_lines("day1.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(calibration) = line {
+                println!("{}", calibration);
+                part1 += extract_calibration_value(&calibration);
+                part2 += process_calibration_with_words(&calibration);
+            }
+        }
+    }
+    println!("part 1: {}", part1);
+    println!("part 2: {}", part2);
+}
+
+fn extract_calibration_value(input: &str) -> i32 {
+    let mut result: Vec<char> = Vec::new();
+    for c in input.chars() {
+        if c.is_numeric() {
+            result.push(c);
+            break;
+        }
+    }
+
+    for c in input.chars().rev() {
+        if c.is_numeric() {
+            result.push(c);
+            break;
+        }
+    }
+    // println!("{}", result.join(""));
+    format!("{}{}", result.get(0).unwrap(), result.get(1).unwrap())
+        .parse::<i32>()
+        .unwrap()
+}
+
+fn process_calibration_with_words(input: &str) -> i32 {
+    let re = Regex::new(r"([1-9]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    // let results: Vec<&str> = re.find_iter(input).map(|m| m.as_str()).collect();
+    // let mut results: Vec<&str> = Vec::new();
+    let results: Vec<String> = input
+        .char_indices()
+        .filter_map(|(i, _)| re.captures(&input[i..]))
+        .map(|cap| cap[1].to_string())
+        .collect();
+
+    let processed = format!("{}{}", results.first().unwrap(), results.last().unwrap())
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9");
+
+    extract_calibration_value(processed.as_str())
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day1::{extract_calibration_value, process_calibration_with_words};
+
+    #[test]
+    fn part1_demo_input() {
+        assert_eq!(extract_calibration_value("1abc2"), 12);
+        assert_eq!(extract_calibration_value("pqr3stu8vwx"), 38);
+        assert_eq!(extract_calibration_value("a1b2c3d4e5f"), 15);
+        assert_eq!(extract_calibration_value("treb7uchet"), 77);
+        assert_eq!(extract_calibration_value("sevenhcgr6ninefour"), 66);
+    }
+
+    #[test]
+    fn part2_demo_input() {
+        assert_eq!(process_calibration_with_words("two1nine"), 29);
+        assert_eq!(process_calibration_with_words("eightwothree"), 83);
+        assert_eq!(process_calibration_with_words("abcone2threexyz"), 13);
+        assert_eq!(process_calibration_with_words("xtwone3four"), 24);
+        assert_eq!(process_calibration_with_words("4nineeightseven2"), 42);
+        assert_eq!(process_calibration_with_words("zoneight234"), 14);
+        assert_eq!(process_calibration_with_words("7pqrstsixteen"), 76);
+        assert_eq!(
+            process_calibration_with_words("trknlxnv43zxlrqjtwonect"),
+            41
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+use std::time::Instant;
+
+mod day1;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Day1 {},
+    Day2 {},
+    Day3 {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let start = Instant::now();
+    match cli.command {
+        Some(Commands::Day1 {}) => day1::solve(),
+        Some(Commands::Day2 {}) => println!("unsolved"),
+        Some(Commands::Day3 {}) => println!("unsolved"),
+        None => println!("failed"),
+    }
+    let duration = start.elapsed();
+
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
